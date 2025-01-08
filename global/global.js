5778f4b202a6779b61b22e4b8de84613f2314713
Vinh <lytranvinh.work@gmail.com>
2025-01-08 07:37:01 +0700

    [upgrade]: init load var env

--- global/global.js
+++ global/global.js
@@ -1,18 +1,24 @@
-
+// basic
 global.TOKEN
 global.CHANGLE_ID
 global.DISCORD_CLIENT
 global.CHANNEL_LOGGER
 global.RABBITMQ_URL
+global.PORT_SERVER
 global.CHANNEL_RABBITMQ
-
+// logger type
 global.LOGGER_INFO = "info"
 global.LOGGER_WARNING = "waring"
 global.LOGGER_DANGER = "danger"
-
+// api weather
 global.KEY_API_WEATHER
-
+// discord id
 global.APPLICATION_ID
 global.GUILD_ID
-
 global.CHANNEL_ID
+// gg
+global.CLIENT_ID_GG
+global.PROJECT_ID_GG
+global.CLIENT_SECRET_GG
+global.TOKEN_GG
+global.REFRESH_TOKEN_GG
--- init/load_config.js
+++ init/load_config.js
@@ -9,6 +9,12 @@ const load_config = async () => {
     global.KEY_API_WEATHER = process.env.KEY_API_WEATHER;
     global.APPLICATION_ID = process.env.APPLICATION_ID;
     global.GUILD_ID = process.env.GUILD_ID;
+    // load init gg
+    global.CLIENT_ID_GG = process.env.CLIENT_ID_GG;
+    global.PROJECT_ID_GG = process.env.PROJECT_ID_GG;
+    global.CLIENT_SECRET_GG = process.env.CLIENT_SECRET_GG;
+    global.TOKEN_GG = process.env.TOKEN_GG;
+    global.REFRESH_TOKEN_GG = process.env.REFRESH_TOKEN_GG;
 };
 
 export default load_config;
